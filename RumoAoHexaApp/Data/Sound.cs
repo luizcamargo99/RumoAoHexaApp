8c8c5c455dae913762bb5409bbcaec64ff8a6023
Luiz Camargo <luiz.zito@hotmail.com>
2022-11-20 11:43:44 +0000

    Updates

--- RumoAoHexaApp/Data/Sound.cs
+++ RumoAoHexaApp/Data/Sound.cs
@@ -5,6 +5,4 @@ internal class Sound
     public required string DisplayName { get; set; }
     public required string FileName { get; set; }
     public bool Active { get; set; } = false;
-    public required int DurationInSeconds { get; set; }
-    public List<Task> Tasks { get; set; } = new();
 }
